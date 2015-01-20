77162e374f259386a2c94e6d6da550a28cb93d79
Ben Tefay <btefay@gmail.com>
2015-01-21 09:33:07 +1000

    Moved config into separate file

---
+++ RequireJs-Example/client/widget1/js/config.js
@@ -0,0 +1,14 @@
+﻿require.config({
+    baseUrl: "../../",
+    urlArgs: "cachebust=" + (new Date()).getTime(),
+    paths: {
+      js: "widget1/js"
+    },
+    map: {
+        '*': {
+            'css': 'require/plugins/require-css/css',
+            'text': 'require/plugins/text'
+        }
+    },
+    deps: [ "js/main" ]
+});
